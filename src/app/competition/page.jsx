2afeb546084a249907db148ce46da9f85f077cb4
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2024-01-16 17:55:23 +0600

    created the ux or the challenge section and working on the ui of the challenge card

--- src/app/competition/page.jsx
+++ src/app/competition/page.jsx
@@ -1,9 +1,11 @@
+import Challenge from "@/components/Competition/Challenge/Challenge";
 import CompetitionSlider from "@/components/CompetitionSlider/CompetitionSlider";
 
 const page = () => {
     return(
         <div>
             <CompetitionSlider/>
+            <Challenge></Challenge>
         </div>
     )}
 export default page;
--- src/components/Competition/Challenge/Challenge.jsx
+++ src/components/Competition/Challenge/Challenge.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import ChallengeCard from "./ChallengeCard";
 
 const Challenge = () => {
-    return (
-        <div>
-            hello i am challenge container
-        </div>
-    );
+  const challenges = ["ch1", "ch2", "ch3", "ch4", "ch5", "ch6"];
+  return (
+    <div className="mx-auto my-24 container px-2">
+      <h2 className="text-3xl font-bold mb-4">Choose Your Challenge</h2>
+      <p className="text-gray-600 mb-8">
+        Explore the challenges below and pick the one that inspires you the
+        most. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam vitae sapiente ea eveniet ipsa adipisci pariatur, dolore optio modi tempora!
+      </p>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {challenges.map((challenge) => (
+          <ChallengeCard key={challenge} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Challenge;

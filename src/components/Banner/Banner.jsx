44b80bb42a52c78e08b5210b194e1f13c05d6854
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2024-01-16 19:08:56 +0600

    banner overlay added and faq section added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 const Banner = () => {
   return (
 
-<<<<<<< HEAD
     <div className='relative'>
       <div className='absolute pl-4 md:pl-16 lg:pl-32 bottom-1/2 '>
         <h1 className='text-6xl text-center  text-white '>Lorem ipsum dolor sit amet.</h1>
@@ -12,9 +11,6 @@ const Banner = () => {
   Button
 </button>
       </div>
-=======
-    <div>
->>>>>>> 6538080e435b64a2e8dc3ffb6061e3f7720a162f
       <video src={require('../../assets/bannerVideo.mp4')} autoPlay muted loop />
     </div>
 
@@ -22,7 +18,3 @@ const Banner = () => {
 };
 
 export default Banner;
-<<<<<<< HEAD
-
-=======
->>>>>>> 6538080e435b64a2e8dc3ffb6061e3f7720a162f

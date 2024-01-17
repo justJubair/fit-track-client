6e9cfcfff31a14dd614b2f25cc55e1e60125d4c0
Jubair <jubair.ahmed2838@gmail.com>
2024-01-18 02:28:57 +0600

    Add: banner overlay added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,22 +1,37 @@
-"use client"
-import React from 'react';
-import bannerVideo from "../../assets/videos/bannerVideo.mp4"
-import { Box, Button, Typography } from '@mui/material';
+"use client";
+import React from "react";
+import bannerVideo from "../../assets/videos/bannerVideo.mp4";
+import { Box, Button, Typography } from "@mui/material";
 
 const Banner = () => {
   return (
-
     <Box position="relative">
-        <video  height="100vh" width="100%" object src={bannerVideo} autoPlay muted loop />
-        {/* overlay */}
-        <Box position="absolute" height="100vh" width="100%"></Box>
+      <video
+        height="100vh"
+        width="100%"
+        object
+        src={bannerVideo}
+        autoPlay
+        muted
+        loop
+      />
+      {/* overlay */}
+      <Box position="absolute" height="100vh" width="100%" top="0" bgcolor="rgba(0, 0, 0, 0.5)"></Box>
       <Box position="absolute" top="30%">
-        <Typography variant='h2' color="white">Lorem ipsum dolor sit amet.</Typography>
-        <Button variant='outlined'>Explore </Button>
+        <Typography
+          variant="h2"
+          style={{
+            background: "-webkit-linear-gradient(45deg, #1FD5F7 30%, white 90%)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+          }}
+          fontWeight="bold"
+        >
+          Power Up Your Potential with FitTrack
+        </Typography>
+        <Button variant="outlined">Explore </Button>
       </Box>
-    
     </Box>
-
   );
 };
 

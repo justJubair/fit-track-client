18dbfe0a01e3255de10ad336f92fe03c781ea443
Saidul Arefin <saidularefin8@gmail.com>
2024-01-18 10:51:11 +0600

    saving the changes

--- src/components/Competition/Challenge/Challenge.jsx
+++ src/components/Competition/Challenge/Challenge.jsx
@@ -8,7 +8,9 @@ const Challenge = () => {
       <h2 className="text-3xl font-bold mb-4">Choose Your Challenge</h2>
       <p className="text-gray-600 mb-8">
         Explore the challenges below and pick the one that inspires you the
-        most. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam vitae sapiente ea eveniet ipsa adipisci pariatur, dolore optio modi tempora!
+        most. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        vitae sapiente ea eveniet ipsa adipisci pariatur, dolore optio modi
+        tempora!
       </p>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
         {challenges.map((challenge) => (

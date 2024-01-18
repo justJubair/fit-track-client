3114c1e9b57a834cefed198411e498e8c8992210
ArnabTo <arnabs929@gmail.com>
2024-01-18 15:29:52 +0600

    [login]:implemented static login page

---
+++ src/app/(site)/usercheck/page.jsx
@@ -0,0 +1,78 @@
+'use client';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { useRouter } from 'next/navigation';
+
+const Usercheck = () => {
+  const [isUser, setUser] = React.useState(true);
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    console.log('Email:', email);
+    console.log('Password:', password);
+    if (isUser) {
+      router.push('/login')
+      alert('user ase')
+    } else {
+      alert('user nai')
+    }
+  }
+
+
+  return (
+    <div className='my-12'>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={(e) => handleSubmit(e)} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2, color:'#252525' }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </div>
+  );
+};
+
+export default Usercheck;

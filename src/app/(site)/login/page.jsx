ec09c04a228751650dab2fb3779e5cbdf2f4ea00
ArnabTo <arnabs929@gmail.com>
2024-01-19 16:28:45 +0600

    [login]:updated login signup page codes

--- src/app/(site)/login/page.jsx
+++ src/app/(site)/login/page.jsx
@@ -1,95 +1,12 @@
-/* eslint-disable react/no-unescaped-entities */
-'use client';
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import Login_Pass from "@/components/Authentication/Login_Password/Login_Pass";
+
+
 
 const Login = () => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const email = formData.get('email');
-        const password = formData.get('password');
-        console.log('Email:', email);
-        console.log('Password:', password);
-        if (isUser) {
-            alert('user ase')
-        } else {
-            alert('user nai')
-        }
-    }
-
     return (
         <div className='mt-36 mb-36'>
-            <Container component="main" maxWidth="md">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 9,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Typography component="h1" variant="h4" sx={{ width: { xs: '100%', md: '50%' }, color: '#252525', fontWeight: '600', marginTop: '25px' }}>
-                        Enter your password!
-                    </Typography>
-                    <Box component="form" onSubmit={(e) => handleSubmit(e)} noValidate sx={{ mt: 1, width: { xs: '100%', md: '50%' } }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                        />
-                        <Button
-                            className='bg-[#252525] hover:text-[#252525] '
-                            type="submit"
-                            sx={{
-                                mt: 3,
-                                mb: 2,
-                                padding: '10px',
-                                color: '#fff',
-                                width: '50%',
-                                borderRadius: '20px',
-                                float: 'inline-end',
-                                boxShadow: '24px 24px 49px #d9d9d9, -24px -24px 49px #ffffff',
-                            }}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="/register" variant="body2">
-                                    Don't have an account? Sign Up
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-            </Container>
+           <Login_Pass/>
         </div>
     );
 };

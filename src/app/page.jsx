38eeddfea4e2bf5e3715dde6d992748cb27790c0
Jubair <jubair.ahmed2838@gmail.com>
2024-01-23 13:52:53 +0600

    Add: navbar removed for grouping route

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
             <Banner/>
             <Services />
             <FAQ></FAQ>
-            <Advertisement />
+            <Advertisement/>
             <Poster></Poster>
             <AiChatbot/>
         </div>

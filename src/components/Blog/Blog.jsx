ccd4a059b0c1b0990dba16e9bf6b6927d020d749
Jubair <jubair.ahmed2838@gmail.com>
2024-01-23 22:15:34 +0600

    Bugfix: warnings removed from blog page

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -54,9 +54,9 @@ const Blog = () => {
                     {/* image input file */}
                     <div className="flex items-center gap-2  border w-32 mx-auto rounded-lg p-2 border-black mt-6">
                         <p>IMAGE</p>
-                        <div class="input-div">
-                            <input type="file" class="input" name="file" />
-                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round" stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon"><polyline points="16 16 12 12 8 16"></polyline><line y2="21" x2="12" y1="12" x1="12"></line><path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path><polyline points="16 16 12 12 8 16"></polyline></svg>
+                        <div className="input-div">
+                            <input type="file" className="input" name="file" />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" strokeLinejoin="round" strokeLinecap="round" viewBox="0 0 24 24" strokeWidth="2" fill="none" stroke="currentColor" className="icon"><polyline points="16 16 12 12 8 16"></polyline><line y2="21" x2="12" y1="12" x1="12"></line><path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path><polyline points="16 16 12 12 8 16"></polyline></svg>
                         </div>
                     </div>
                 </div>

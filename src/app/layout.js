e93299985742c34a5f17dfbaff5599fb2cf5ad1c
ArnabTo <arnabs929@gmail.com>
2024-01-24 17:07:00 +0600

    [auth]:implemented google login

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Footer from "@/components/Shared/Footer/Footer";
 import Nav from "@/components/Shared/Nav/Nav";
 import SmoothScroll from "@/components/SmoothScroll/SmoothScroll";
+import AuthProvider from "@/components/Authentication/AuthProvider/AuthProvider";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,9 +17,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-          <Nav/>
+          <Nav />
+          <AuthProvider>
           {children}
-          <Footer/>
+          </AuthProvider>
+          <Footer />
       </body>
     </html>
   );
--- src/components/Authentication/Sign_Up/SignUp.jsx
+++ src/components/Authentication/Sign_Up/SignUp.jsx
@@ -11,13 +11,18 @@ import Backdrop from '@mui/material/Backdrop';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Grid from '@mui/material/Grid';
-import Link from 'next/link';
+import Divider from '@mui/material/Divider';
+import Chip from '@mui/material/Chip';
+import GoogleIcon from '@mui/icons-material/Google';
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const SignUp = () => {
-
+     
+    const {data: session} = useSession();
+    console.log(session)
     const [isUser, setUser] = React.useState(false);
     const router = useRouter();
-
+    const [value, setValue] = React.useState(0);
     const style = {
         position: 'absolute',
         top: '50%',
@@ -52,111 +57,129 @@ const SignUp = () => {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
+    const handleSignUp = () =>{
+        signIn()
+        if(session){
+           router.push('/')
+        }
+    }
+
     return (
         <Container component="main" maxWidth="md">
-        <CssBaseline />
-        <Box
-            sx={{
-                marginTop: 8,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-            }}
-        >
-            <Typography component="h1" variant="h4" sx={{ width: { xs: '100%', md: '50%' }, color: '#252525', fontWeight: '600', marginTop: '25px' }}>
-                Create your account & join with us!
-            </Typography>
-            <Box component="form" onSubmit={(e) => handleSubmit(e)} noValidate sx={{ mt: 1, width: { xs: '100%', md: '50%' } }}>
-                <Grid container spacing={2}>
-                    <Grid item xs>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="firstname"
-                            label="First Name"
-                            type="text"
-                            id="name"
-                        />
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="lastname"
-                            label="Last Name"
-                            type="text"
-                            id="lastName"
-                        />
-                    </Grid>
-                </Grid>
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
-                <Modal
-                    aria-labelledby="transition-modal-title"
-                    aria-describedby="transition-modal-description"
-                    open={open}
-                    onClose={handleClose}
-                    closeAfterTransition
-                    slots={{ backdrop: Backdrop }}
-                    slotProps={{
-                        backdrop: {
-                            timeout: 500,
-                        },
-                    }}
-                >
-                    <Fade in={open}>
-                        <Box sx={style}>
-                            <Typography id="transition-modal-title" variant="h6" component="h2">
-                                Terms & Conditions
-                            </Typography>
-                            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                                Must Have to be human.
-                            </Typography>
-                        </Box>
-                    </Fade>
-                </Modal>
-                <Typography component="h6" variant="h6" sx={{ fontSize: '16px', color: 'gray', fontWeight: '400', marginTop: '25px' }}>
-                    By continuing, <span className='cursor-pointer underline' onClick={handleOpen}> I agree to Nike’s Privacy Policy and Terms of Use.</span>
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography component="h1" variant="h4" sx={{ width: { xs: '100%', md: '50%' }, color: '#378AE5', fontWeight: '600', marginTop: '25px' }}>
+                    Create your account & join with us!
                 </Typography>
-                <Button
-                    className='bg-[#252525] hover:text-[#252525] '
-                    type="submit"
-                    sx={{
-                        mt: 3,
-                        mb: 2,
-                        padding: '10px',
-                        color: '#fff',
-                        width: '50%',
-                        borderRadius: '20px',
-                        float: 'inline-end',
-                        boxShadow: '24px 24px 49px #d9d9d9, -24px -24px 49px #ffffff',
-                    }}
-                >
-                    Continue
-                </Button>
+                <Box component="form" onSubmit={(e) => handleSubmit(e)} noValidate sx={{ mt: 1, mb: 3, width: { xs: '100%', md: '50%' } }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="firstname"
+                                label="First Name"
+                                type="text"
+                                id="name"
+                            />
+                        </Grid>
+                        <Grid item>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="lastname"
+                                label="Last Name"
+                                type="text"
+                                id="lastName"
+                            />
+                        </Grid>
+                    </Grid>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <Modal
+                        aria-labelledby="transition-modal-title"
+                        aria-describedby="transition-modal-description"
+                        open={open}
+                        onClose={handleClose}
+                        closeAfterTransition
+                        slots={{ backdrop: Backdrop }}
+                        slotProps={{
+                            backdrop: {
+                                timeout: 500,
+                            },
+                        }}
+                    >
+                        <Fade in={open}>
+                            <Box sx={style}>
+                                <Typography id="transition-modal-title" variant="h6" component="h2">
+                                    Terms & Conditions
+                                </Typography>
+                                <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                                    Must Have to be human.
+                                </Typography>
+                            </Box>
+                        </Fade>
+                    </Modal>
+                    <Typography component="h6" variant="h6" sx={{ fontSize: '16px', color: 'gray', fontWeight: '400', marginTop: '25px' }}>
+                        By continuing, <span className='cursor-pointer underline' onClick={handleOpen}> I agree to Nike’s Privacy Policy and Terms of Use.</span>
+                    </Typography>
+                    <Button
+                        className='bg-[#252525] hover:text-[#252525] '
+                        type="submit"
+                        sx={{
+                            mt: 3,
+                            mb: 2,
+                            padding: '10px',
+                            color: '#fff',
+                            width: '50%',
+                            borderRadius: '20px',
+                            float: 'inline-end',
+                            boxShadow: '24px 24px 49px #d9d9d9, -24px -24px 49px #ffffff',
+                        }}
+                    >
+                        Continue
+                    </Button>
+                </Box>
+                <Divider>
+                    <Chip label="Or" size="large" />
+                </Divider>
+                <Box>
+                    <Typography component="h6" variant="h6" sx={{ fontSize: '16px', color: 'gray', fontWeight: '400' }}>
+                        Continue with,
+                    </Typography>
+                    <Box sx={{ width: 500 }}>
+                    <Button onClick={() => handleSignUp()} variant="outlined"><GoogleIcon/> Continue with google</Button>
+                    </Box>
+                </Box>
             </Box>
-        </Box>
-    </Container>
+        </Container>
     );
 };
 

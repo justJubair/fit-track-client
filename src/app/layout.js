863976949eeac3a36d25854be9ae78910a8eaf17
ArnabTo <arnabs929@gmail.com>
2024-01-24 13:36:53 +0600

    updating branch

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,11 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SmoothScroll>
-          <Nav />
+          <Nav/>
           {children}
-          <Footer />
-        </SmoothScroll>
+          <Footer/>
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,15 +3,16 @@ import Banner from "@/components/Banner/Banner";
 import FAQ from "@/components/FAQ/FAQ";
 import Poster from "@/components/Poster/Poster";
 import Services from "@/components/Services/Services";
+import SmoothScroll from "@/components/SmoothScroll/SmoothScroll";
 
 const page = () => {
     return (
         <div>
-            <Banner/>
-            <Services />
-            <FAQ></FAQ>
-            <Advertisement />
-            <Poster></Poster>
+                <Banner />
+                <Services />
+                <FAQ></FAQ>
+                <Advertisement />
+                <Poster></Poster> 
         </div>
     )
 }

fbe26e00977f65a4ea0beb14a808c1533a54948a
Saidul Arefin <saidularefin8@gmail.com>
2024-01-26 06:52:16 +0600

    [customizediet](pdfdiet):added pdf download feature for the diet plan

--- src/components/CustomizeDiet/DietTable/DietTable.jsx
+++ src/components/CustomizeDiet/DietTable/DietTable.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   Table,
   TableBody,
@@ -10,21 +10,56 @@ import {
   Paper,
   Typography,
   Avatar,
+  Button,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import Image from "next/image";
 import LOGO from "../../../assets/images/logo04.png";
 import { useSession } from "next-auth/react";
 import getDietPlan from "@/app/api/get/getDietPlan";
+import Loader from "@/components/Shared/Loader/Loader";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import { useRouter } from "next/navigation";
+
 
 const DietTable = () => {
   const { data: session } = useSession();
   const email = session?.user?.email;
   const image = session?.user?.image;
   const name = session?.user?.name;
-  console.log(session.user)
+  console.log(session.user);
   const [dietPlanData, setDietPlanData] = useState(null);
   const [loader, setLoader] = useState(true);
+  const pdfRef = useRef();
+  const router = useRouter();
+  const downloadPDF = () => {
+    const input = pdfRef.current;
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4", true);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+      const imgX = (pdfWidth - imgWidth * ratio) / 2;
+      const imgY = (pdfHeight - imgHeight * ratio) / 2;
+
+      pdf.addImage(
+        imgData,
+        "PNG",
+        imgX,
+        imgY,
+        imgWidth * ratio,
+        imgHeight * ratio
+      );
+      pdf.save("Diet-plan.pdf");
+    });
+  };
+  const goHome = () => {
+    router.push("/");
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,12 +68,12 @@ const DietTable = () => {
           const planData = await getDietPlan(email);
           setDietPlanData(planData);
           console.log(planData);
-          setLoader(false); // Set loader to false once data is loaded
+          setLoader(false);
         }
       } catch (error) {
         // Handle errors
         console.error("Error fetching diet plan:", error.message);
-        setLoader(false); // Set loader to false in case of an error
+        setLoader(false);
       }
     };
 
@@ -46,15 +81,12 @@ const DietTable = () => {
   }, [email]);
 
   if (loader) {
-    // Render loading state
-    return <div>Loading...</div>;
+    return <Loader></Loader>;
   }
 
   if (!session || !dietPlanData) {
-    // Render additional loading state or redirect to login page
-    return <div>Loading...</div>;
+    return <Loader></Loader>;
   }
-
 
   return (
     <div
@@ -62,107 +94,116 @@ const DietTable = () => {
         textAlign: "center",
         maxWidth: "600px",
         margin: "20px auto",
-        border: "1px solid gray",
-        borderRadius: "50px",
       }}
     >
-      <Box
-        sx={{
-          mx: "2rem",
-          my: "2rem",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-evenly",
-          alignItems: "center",
+      <div
+        ref={pdfRef}
+        style={{
+          textAlign: "center",
+          maxWidth: "600px",
+          margin: "20px auto",
+          border: "1px solid gray",
+          borderRadius: "50px",
         }}
       >
-            <Avatar sx={{ backgroundColor: "black" }} aria-label="recipe">
-          <Image
-            width={500}
-            height={500}
-            src={image}
-            alt=""
-          />
-        </Avatar>
-        <Typography variant="h6" sx={{ color: "black" }}>
-         {name}
-        </Typography>
-        <Typography variant="h6" sx={{ color: "black" }}>
-         {email}
-        </Typography>
-      </Box>
+        <Box
+          sx={{
+            mx: "2rem",
+            my: "2rem",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ backgroundColor: "black" }} aria-label="recipe">
+            <Image width={500} height={500} src={image} alt="" />
+          </Avatar>
+          <Typography variant="h6" sx={{ color: "black" }}>
+            {name}
+          </Typography>
+          <Typography variant="h6" sx={{ color: "black" }}>
+            {email}
+          </Typography>
+        </Box>
 
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Meal</TableCell>
-              <TableCell>Food</TableCell>
-              <TableCell>Quantity</TableCell>
-              <TableCell>Unit</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {dietPlanData.meal_plan.map((meal, mealIndex) => (
-              <React.Fragment key={mealIndex}>
-                <TableRow>
-                  <TableCell
-                    sx={{ fontWeight: "bold" }}
-                    rowSpan={meal.foods.length + 1}
-                  >
-                    {meal.meal}
-                  </TableCell>
-                </TableRow>
-                {meal.foods.map((food, foodIndex) => (
-                  <TableRow key={foodIndex}>
-                    <TableCell>{food.food}</TableCell>
-                    <TableCell>{food.quantity}</TableCell>
-                    <TableCell>{food.unit}</TableCell>
-                  </TableRow>
-                ))}
-              </React.Fragment>
-            ))}
-            <TableRow>
-              <TableCell colSpan={4} style={{ marginTop: "20px", border: "0" }}>
-                <Table>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Meal</TableCell>
+                <TableCell>Food</TableCell>
+                <TableCell>Quantity</TableCell>
+                <TableCell>Unit</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {dietPlanData.meal_plan.map((meal, mealIndex) => (
+                <React.Fragment key={mealIndex}>
                   <TableRow>
-                    <TableCell sx={{ fontWeight: "bold" }} rowSpan={5}>
-                      Nutritional Information:
+                    <TableCell
+                      sx={{ fontWeight: "bold" }}
+                      rowSpan={meal.foods.length + 1}
+                    >
+                      {meal.meal}
                     </TableCell>
                   </TableRow>
-                  <TableRow>
-                    <TableCell>Calories</TableCell>
-                    <TableCell>{dietPlanData.calories}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>Protein</TableCell>
-                    <TableCell>{dietPlanData.protein_grams}g</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>Carbs</TableCell>
-                    <TableCell>{dietPlanData.carbs_grams}g</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>Fat</TableCell>
-                    <TableCell>{dietPlanData.fat_grams}g</TableCell>
-                  </TableRow>
-                </Table>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Box
-        sx={{
-          mb: "2rem",
-          mx: "2rem",
-          display: "flex",
-          justifyContent: "end",
-        }}
-        id="scroll-dialog-title"
-      >
-        <Image src={LOGO} width={150} height={150} alt="Logo" />
-      </Box>
+                  {meal.foods.map((food, foodIndex) => (
+                    <TableRow key={foodIndex}>
+                      <TableCell>{food.food}</TableCell>
+                      <TableCell>{food.quantity}</TableCell>
+                      <TableCell>{food.unit}</TableCell>
+                    </TableRow>
+                  ))}
+                </React.Fragment>
+              ))}
+              <TableRow>
+                <TableCell
+                  colSpan={4}
+                  style={{ marginTop: "20px", border: "0" }}
+                >
+                  <Table>
+                    <TableRow>
+                      <TableCell sx={{ fontWeight: "bold" }} rowSpan={5}>
+                        Nutritional Information:
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>Calories</TableCell>
+                      <TableCell>{dietPlanData.calories}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>Protein</TableCell>
+                      <TableCell>{dietPlanData.protein_grams}g</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>Carbs</TableCell>
+                      <TableCell>{dietPlanData.carbs_grams}g</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>Fat</TableCell>
+                      <TableCell>{dietPlanData.fat_grams}g</TableCell>
+                    </TableRow>
+                  </Table>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Box
+          sx={{
+            mb: "2rem",
+            mx: "2rem",
+            display: "flex",
+            justifyContent: "end",
+          }}
+          id="scroll-dialog-title"
+        >
+          <Image src={LOGO} width={150} height={150} alt="Logo" />
+        </Box>
+      </div>
+      <Button onClick={goHome}>GO HOME</Button>
+      <Button onClick={downloadPDF}>Download PDf</Button>
     </div>
   );
 };

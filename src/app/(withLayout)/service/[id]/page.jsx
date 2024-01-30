9c42ee8067601d192b458d638760678ef7b8ae82
Jubair <jubair.ahmed2838@gmail.com>
2024-01-30 20:15:41 +0600

    Add: navbar adjusted on service detail page

--- src/app/(withLayout)/service/[id]/page.jsx
+++ src/app/(withLayout)/service/[id]/page.jsx
@@ -1,7 +1,9 @@
+import ServiceDetails from "@/components/ServiceDetails/ServiceDetails"
+
 const ServicePage = ({params}) => {
   return (
     <>
-      <h1>This is dynamic service page {params.id}</h1>
+     <ServiceDetails/>
     </>
   )
 }
---
+++ src/components/ServiceDetails/ServiceDetails.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Box } from "@mui/system";
+
+const ServiceDetails = () => {
+    return(
+        <>
+        <Box height="64px" bgcolor="black"></Box>
+             <p> HELLO I Am ServiceDetails </p>
+        </>
+    )}
+export default ServiceDetails;

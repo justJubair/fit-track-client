e5c14e406435e53a2b0acfa5fc04b3e360377db9
ArnabTo <arnabs929@gmail.com>
2024-01-30 20:33:16 +0600

    [price]:updated pricing section

---
+++ src/app/(withLayout)/pricing/page.jsx
@@ -0,0 +1,13 @@
+import Prices from '@/components/Pricing/Pricing';
+
+import React from 'react';
+
+const PricingComponent = () => {
+    return (
+        <div>
+            <Prices/>
+        </div>
+    );
+};
+
+export default PricingComponent;
--- src/app/pricing/page.jsx
+++
@@ -1,9 +0,0 @@
-import Prices from "@/components/Pricing/Pricing";
-
-const Pricing = () => {
-    return(
-        <div className="max-w-6xl mx-auto my-40">
-             <Prices></Prices>
-        </div>
-    )}
-export default Pricing;

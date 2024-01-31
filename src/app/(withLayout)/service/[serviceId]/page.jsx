6b52dfd11aa6044b6444ebc264606f2ca9755b3d
Jubair <jubair.ahmed2838@gmail.com>
2024-01-31 14:32:57 +0600

    Add: service detail banner image added

---
+++ src/app/(withLayout)/service/[serviceId]/page.jsx
@@ -0,0 +1,9 @@
+import ServiceDetails from "@/components/ServiceDetails/ServiceDetails";
+
+const ServiceDetailPage = () => {
+    return(
+        <>
+            <ServiceDetails/>
+        </>
+    )}
+export default ServiceDetailPage;

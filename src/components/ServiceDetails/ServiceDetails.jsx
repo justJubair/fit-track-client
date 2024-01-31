ad71a6bf6abfa188164d95ca0a61d09032b6a550
Jubair <jubair.ahmed2838@gmail.com>
2024-01-31 17:36:51 +0600

    Add: service detail page dummy banner added

--- src/components/ServiceDetails/ServiceDetails.jsx
+++ src/components/ServiceDetails/ServiceDetails.jsx
@@ -1,12 +1,23 @@
 "use client"
 
 import { Box } from "@mui/system";
+import Image from "next/image";
 
 const ServiceDetails = () => {
     return(
         <>
-        <Box height="64px" bgcolor="black"></Box>
-             <p> HELLO I Am ServiceDetails </p>
+        {/* <Box height="64px" bgcolor="black"></Box> */}
+        <Box position="relative" height="100vh">
+        <Image src="https://raw.githubusercontent.com/justJubair/fit-track-client/service-detail-page-jubair/src/assets/images/serviceDetailBanner.jpg?token=GHSAT0AAAAAACJ2ZZJBXVVCTLB3BOFUS7SMZN2AY3A"
+              fill
+              priority
+              style={{
+                objectFit: "cover",
+                
+              }} alt="service detail page banner"/>
+        </Box>
+        {/* overlay */}
+       <div className="absolute h-screen w-full top-0 left-0 bg-black/70"></div>
         </>
     )}
 export default ServiceDetails;

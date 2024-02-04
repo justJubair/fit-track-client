6e356ebf5aef242267d0b830b26a2b48a54c9d20
Aurnab <zubayeraurnab@gmail.com>
2024-02-04 20:55:54 +0600

    details btn added

--- src/components/Blog/BlogCard.jsx
+++ src/components/Blog/BlogCard.jsx
@@ -2,12 +2,13 @@
 import BookmarksIcon from '@mui/icons-material/Bookmarks';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-import { IconButton, Tooltip } from "@mui/material";
+import { Button, IconButton, Tooltip } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import Typography from "@mui/material/Typography";
+import { Box, Stack } from '@mui/system';
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
@@ -55,9 +56,11 @@ const BlogCard = ({ challenge }) => {
     }
     return (
         <div className="lg:w-4/5 mx-auto">
-            <Link href={"blog/blogDetails"}>
-                <Card sx={{ backgroundColor: 'white' }}>
-                    <Image width={500} height={500} src={challenge?.image} alt="Card Image" className="w-full h-96 object-cover" />
+
+            <Card sx={{ backgroundColor: 'white' }}>
+                <Image width={500} height={500} src={challenge?.image} alt="Card Image" className="w-full h-96 object-cover" />
+
+                <div className='flex justify-between mt-2 items-center '>
                     <CardHeader
                         avatar={
                             <Avatar sx={{ backgroundColor: 'black' }} aria-label="recipe">
@@ -70,52 +73,56 @@ const BlogCard = ({ challenge }) => {
                         subheader={challenge?.time}
                         sx={{ title: { color: 'black' } }}
                     />
-                    <CardContent>
-                        <Typography variant="h6" sx={{ color: 'black' }}>
-                            {
-                                challenge?.title?.length > 40 ?
-                                    challenge?.title.slice(0, 40) + "..."
-                                    :
-                                    challenge?.title
-                            }
-                        </Typography>
-                        <Typography variant="body2" sx={{ color: 'black' }}>
-                            {challenge.description.length > 250
-                                ? challenge.description.slice(0, 250).concat("...")
-                                : challenge.description
-                            }
-                        </Typography>
-                    </CardContent>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', padding: '0.5rem 1rem' }}>
-                        <Tooltip title="Save" arrow>
-                            <IconButton aria-label="add to favorites">
-                                <BookmarksIcon />
-                            </IconButton>
-                        </Tooltip>
-                        <div className="space-x-4 flex items-center">
-                            <div className="space-y-1">
-                                <Tooltip title="Like" arrow>
-                                    <IconButton onClick={() => handelLikeBtn(challenge?._id)} aria-label="add to favorites">
-                                        <FitnessCenterIcon sx={{ mr: "10px" }} />
-                                        <Typography>{challenge?.likes.length}</Typography>
-                                    </IconButton>
-                                </Tooltip>
-                            </div>
-                            <div className="border h-8 border-black"></div>
-                            <div className="space-y-1">
-                                <Tooltip title="Dislike" arrow >
-                                    <IconButton onClick={() => handelDisLikeBtn(challenge?._id)} aria-label="add to favorites">
-                                        <FastfoodIcon sx={{ mr: "10px" }} />
-                                        <Typography>{challenge?.disLikes.length}</Typography>
-                                    </IconButton>
-                                </Tooltip>
-                            </div>
+                    <Link href={"blog/blogDetails"}>
+                        <Button size='small' color='inherit' variant="outlined" sx={{ mr: '10px' }}>Details</Button>
+                    </Link>
+                </div>
+                <CardContent>
+                    <Typography variant="h6" sx={{ color: 'black' }}>
+                        {
+                            challenge?.title?.length > 40 ?
+                                challenge?.title.slice(0, 40) + "..."
+                                :
+                                challenge?.title
+                        }
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'black' }}>
+                        {challenge.description.length > 250
+                            ? challenge.description.slice(0, 250).concat("...")
+                            : challenge.description
+                        }
+                    </Typography>
+                </CardContent>
+                <div style={{ display: 'flex', justifyContent: 'space-between', padding: '0.5rem 1rem' }}>
+                    <Tooltip title="Save" arrow>
+                        <IconButton aria-label="add to favorites">
+                            <BookmarksIcon />
+                        </IconButton>
+                    </Tooltip>
+                    <div className="space-x-4 flex items-center">
+                        <div className="space-y-1">
+                            <Tooltip title="Like" arrow >
+                                <IconButton onClick={() => handelLikeBtn(challenge?._id)} aria-label="add to favorites">
+                                    <FitnessCenterIcon sx={{ mr: "10px" }} />
+                                    <Typography>{challenge?.likes.length}</Typography>
+                                </IconButton>
+                            </Tooltip>
                         </div>
-
+                        <div className="border h-8 border-black"></div>
+                        <div className="space-y-1">
+                            <Tooltip title="Dislike" arrow >
+                                <IconButton onClick={() => handelDisLikeBtn(challenge?._id)} aria-label="add to favorites">
+                                    <FastfoodIcon sx={{ mr: "10px" }} />
+                                    <Typography>{challenge?.disLikes.length}</Typography>
+                                </IconButton>
+                            </Tooltip>
+                        </div>
                     </div>
 
-                </Card>
-            </Link>
+                </div>
+
+            </Card>
+
         </div>
     );
 };

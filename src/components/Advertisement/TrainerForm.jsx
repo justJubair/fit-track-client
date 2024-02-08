a04679ec418872ee8cede90f5f1f53d794989864
Jubair <jubair.ahmed2838@gmail.com>
2024-02-08 18:04:07 +0600

    Add: dummy form created

--- src/components/Advertisement/TrainerForm.jsx
+++ src/components/Advertisement/TrainerForm.jsx
@@ -1,3 +1,4 @@
+"use client"
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
@@ -6,6 +7,12 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
+import { Controller, useForm } from 'react-hook-form';
+import { FormControl, FormControlLabel, Radio, RadioGroup, TextField, Typography } from '@mui/material';
+import Image from 'next/image';
+
+// import logo
+import LOGO from "../../assets/images/logo02.png"
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -18,9 +25,28 @@ const TrainerForm =({open, setOpen})=> {
 //     setOpen(true);
 //   };
 
+const {
+  control,
+  handleSubmit,
+  formState: { errors },
+} = useForm();
+
   const handleClose = () => {
     setOpen(false);
   };
+
+  const onSubmit = (data) => {
+    const formData = {
+      username: username,
+      email: email,
+      ...data,
+    };
+
+    // submitHealthInfo(formData);
+    // handleClose();
+  };
+
+
 
   return (
     <React.Fragment>
@@ -34,12 +60,160 @@ const TrainerForm =({open, setOpen})=> {
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+          <DialogTitle
+            sx={{ mx: "auto", display: "flex", justifyContent: "center" }}
+            id="scroll-dialog-title"
+          >
+            <Image src={LOGO} width={150} sx={{ height: "auto" }} alt="Logo" />
+          </DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
+        <form
+              onSubmit={handleSubmit(onSubmit)}
+              style={{ width: "100%", marginTop: 10 }}
+            >
+              <Controller
+                name="age"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: "Age is required",
+                  pattern: {
+                    value: /^\d+$/,
+                    message: "Please enter a valid age (Numbers only)",
+                  },
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="Age"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    error={!!errors.age}
+                    helperText={errors.age?.message}
+                    InputProps={{
+                      style: { color: "#fff" },
+                    }}
+                    InputLabelProps={{
+                      style: { color: "#fff" },
+                    }}
+                    sx={{
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#fff",
+                      },
+                    }}
+                  />
+                )}
+              />
+              <Controller
+                name="weight"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: "Weight is required",
+                  pattern: {
+                    value: /^\d+$/,
+                    message: "Please enter a valid weight (Numbers only)",
+                  },
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="Weight (kg)"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    error={!!errors.weight}
+                    helperText={errors.weight?.message}
+                    InputProps={{
+                      style: { color: "#fff" },
+                    }}
+                    InputLabelProps={{
+                      style: { color: "#fff" },
+                    }}
+                    sx={{
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#fff",
+                      },
+                    }}
+                  />
+                )}
+              />
+
+              <Controller
+                name="height"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: "Height is required",
+                  pattern: {
+                    value: /^\d+$/,
+                    message: "Please enter a valid height (Numbers only)",
+                  },
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="Height (cm)"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    error={!!errors.height}
+                    helperText={errors.height?.message}
+                    InputProps={{
+                      style: { color: "#fff" },
+                    }}
+                    InputLabelProps={{
+                      style: { color: "#fff" },
+                    }}
+                    sx={{
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#fff",
+                      },
+                    }}
+                  />
+                )}
+              />
+
+              <FormControl component="fieldset" margin="normal">
+                <Typography variant="subtitle1" style={{ color: "#fff" }}>
+                  Current Fitness Level
+                </Typography>
+                <Controller
+                  name="fitnessLevel"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: "Fitness level is required" }}
+                  render={({ field }) => (
+                    <RadioGroup {...field} row>
+                      <FormControlLabel
+                        value="beginner"
+                        control={<Radio style={{ color: "#fff" }} />}
+                        label="Beginner"
+                        style={{ color: "#fff" }}
+                      />
+                      <FormControlLabel
+                        value="intermediate"
+                        control={<Radio style={{ color: "#fff" }} />}
+                        label="Intermediate"
+                        style={{ color: "#fff" }}
+                      />
+                      <FormControlLabel
+                        value="advanced"
+                        control={<Radio style={{ color: "#fff" }} />}
+                        label="Advanced"
+                        style={{ color: "#fff" }}
+                      />
+                    </RadioGroup>
+                  )}
+                />
+                {errors.fitnessLevel && (
+                  <Typography color="error">
+                    {errors.fitnessLevel.message}
+                  </Typography>
+                )}
+              </FormControl>
+            </form>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Disagree</Button>

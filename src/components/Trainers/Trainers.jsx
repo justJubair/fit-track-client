3ebd225ad5ae8896cca3bf320b8ec7a603f9ec19
ArnabTo <arnabs929@gmail.com>
2024-02-08 11:11:06 +0600

    trainer-arnab

--- src/components/Trainers/Trainers.jsx
+++ src/components/Trainers/Trainers.jsx
@@ -1,40 +1,60 @@
 'use client';
 import { Avatar, Box, CardContent, Chip, Grid, Typography, CardOverflow, CardActions, ButtonGroup, Button, Card } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
+import TrainerCard from "./TrainerCard";
 
 const Trainers = ({ allTrainers }) => {
-    console.log(allTrainers)
+    // console.log(allTrainers)
+
     return (
-        <Box>
-            <Box className="bg-black h-16"></Box>
-            <Box sx={{ maxWidth: '1336px', mx: 'auto', my: 3 }}>
+        <div>
+            <div className="bg-black h-16"></div>
+            <div className="max-w-6xl mx-auto my-6">
                 <Typography variant="h4" sx={{ width: 1 / 2, textAlign: 'center', fontWeight: '500', mx: 'auto', marginBottom: '40px' }}>Get your<span className="text-[#378ae5]">Trainers Here</span></Typography>
-                <Box sx={{ marginLeft: '10px', marginRight: '10px' }}>
-                    <Grid container sx={{ justifyContent: "center" }}>
-                        {
-                            allTrainers.map((train) =>
-                                <Grid item xs={12} md={8} lg={3} key={train._id}>
-                                    <div class="flex items-center justify-center rounded-md">
-                                        <div class="w-64 rounded-lg border-2  border-[#252525] bg-transparent p-4 text-center shadow-lg">
-                                            <div class="mx-auto mb-4 flex h-16 w-16 items-center justify-center rounded-full bg-indigo-500 dark:bg-indigo-600">
-                                                <Image width={100} height={100} src={train.profile_image} />
-                                            </div>
-                                            <h2 class="mt-4 text-xl font-bold text-indigo-400">{train.name}</h2>
-                                            <p class="mb-4 text-black">{train.specialization}</p>
-                                            <div class="flex items-center justify-center gap-4">
-                                                <button className="bg-black px-6 py-2 rounded-[20px] text-white">Connect</button>
-                                                <button className="bg-black px-6 py-2 rounded-[20px] text-white">Details</button>
-                                            </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mx-3">
+                    {
+                        allTrainers.map((train) =>
+                            <div key={train._id}>
+                                <div className="flex items-center justify-center rounded-md">
+                                    <div className="w-64 rounded-lg border-2  border-[#252525] bg-transparent p-4 text-center shadow-lg">
+                                        <span className="flex justify-end">
+                                            {
+                                                train.status === 'active' ?
+                                                    <svg className="w-6 h-6 text-green-500" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <circle cx="12" cy="12" r="8" fill="currentColor" />
+                                                    </svg>
+                                                    :
+                                                    <svg className="w-6 h-6 text-green-500 opacity-0" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <circle cx="12" cy="12" r="8" fill="currentColor" />
+                                                    </svg>
+                                            }
+
+                                        </span>
+                                        <div className="mx-auto mb-4 flex h-16 w-16 items-center justify-center rounded-full -mt-6">
+                                            <Image width={100} height={100} src={train.profile_image} alt='trainer image' />
+                                        </div>
+                                        <h2 className="mt-4 text-xl font-bold text-[#378ae5]">{train.name}</h2>
+                                        <p className="mb-4 text-black">{train.specialization}</p>
+                                        <div className="flex items-center justify-center gap-4">
+                                            <button className="bg-black px-6 py-2 rounded-[20px] text-white transition-transform duration-300 transform hover:scale-90 focus:outline-none active:scale-110">Connect</button>
+                                            <Link href={`/trainers/${train._id}`}><button className="bg-black px-6 py-2 rounded-[20px] text-white transition-transform duration-300 transform hover:scale-90 focus:outline-none active:scale-110">Details</button></Link>
                                         </div>
                                     </div>
+                                   
+                                </div>
 
-                                </Grid>
-                            )
-                        }
-                    </Grid>
-                </Box>
-            </Box>
-        </Box>
+                            </div>
+                        )
+                    }
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mx-3">
+                    {
+                        allTrainers.map((train) => <TrainerCard train={train} key={train._id}></TrainerCard> )
+                    }
+                </div>
+            </div>
+        </div>
     );
 };
 

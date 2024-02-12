25beb849c66fed69c9c88d8fa2b6877a210575d2
Saidul Arefin <saidularefin8@gmail.com>
2024-02-12 22:19:12 +0600

    created dashboard manage challenge route

---
+++ src/app/(dashboardLayout)/manage-challenge/page.jsx
@@ -0,0 +1,10 @@
+import ManageChallenge from "@/components/Dashboard/ManageChallenge/MAnageChallenge";
+
+const ManageChallengePage = async() => {
+    
+    return(
+        <div>
+            <ManageChallenge/>
+        </div>
+    )}
+export default ManageChallengePage;
--- src/components/Competition/Challenge/Challenge.jsx
+++ src/components/Competition/Challenge/Challenge.jsx
@@ -15,23 +15,21 @@ const Challenge = () => {
   }, []);
   return (
     <div className="mx-auto my-24 container ">
-      <div>
+      <div >
         <h2 className="text-3xl font-bold mb-4 px-2">Choose Your Challenge</h2>
         <p className="black mb-8 px-2">
-          Explore the challenges below and pick the one that inspires you the
-          most. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Quibusdam vitae sapiente ea eveniet ipsa adipisci pariatur, dolore
-          optio modi tempora!
+        Explore the challenges below and choose the one that resonates with you. Life's journey is a canvas of opportunities and obstacles, each contributing to your unique story. Embrace challenges as stepping stones to growth and success. Your path is yours to shape, so seize each moment with courage and resilience.
         </p>
+        
       </div>
+      <div className="px-2">
       <div className="grid md:grid-cols-2  gap-6">
         {challenges.map((challenge) => (
           <ChallengeCard key={challenge?.challengeName} challenge={challenge} />
         ))}
       </div>
-      <div className="flex justify-end my-5">
-        <Button variant="outlined">View more Challenges</Button>
       </div>
+      
       <ToastContainer />
     </div>
   );

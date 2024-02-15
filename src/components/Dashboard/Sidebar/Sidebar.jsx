062f9acd8df47ebec340d6e8ac44de709b5ac333
Jubair <jubair.ahmed2838@gmail.com>
2024-02-15 18:42:37 +0600

    Update: sidebar is updated

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -1,10 +1,8 @@
-"use client"
+"use client";
 import { usePathname } from "next/navigation";
 import { Box } from "@mui/system";
 import Image from "next/image";
 import Link from "next/link";
-
-
 
 // import vanilla css
 import "./Sidebar.css";
@@ -16,53 +14,56 @@ import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 import LogoutIcon from "@mui/icons-material/Logout";
 import WidgetsIcon from "@mui/icons-material/Widgets";
 
-
-
-
-
-
 const Sidebar = () => {
-
-  const pathname = usePathname()
-
+  const pathname = usePathname();
 
   return (
-    <Box>
-      {/* logo */}
-      <div className="hidden md:flex justify-center mt-5">
-       <Link href="/">
-       <Image
-          width={30}
-          height={30}
-          priority
-          sx={{ height: "auto" }}
-          src="https://raw.githubusercontent.com/ecemgo/mini-samples-great-tricks/main/fitness-app-dashboard/images/app-logo.png"
-          alt="dashboard-logo"
-        />
-       </Link>
-      </div>
-
-      {/* Routes */}
+    <div className="fixed min-h-screen flex flex-col justify-between">
+      {/* logo and Routes */}
       <div>
-      
-         {/* Route one */}
-         <ul>
-          <li className={pathname == "/dashboard" ? "active nav-item" : "nav-item"}>
-            
-            <Link href="/dashboard" >
+        {/* logo */}
+        <div className="hidden md:flex justify-center pt-5">
+          <Link href="/">
+            <Image
+              width={30}
+              height={30}
+              priority
+              sx={{ height: "auto" }}
+              src="https://raw.githubusercontent.com/ecemgo/mini-samples-great-tricks/main/fitness-app-dashboard/images/app-logo.png"
+              alt="dashboard-logo"
+            />
+          </Link>
+        </div>
+
+        {/* Route one */}
+        <ul>
+          <li
+            className={
+              pathname == "/dashboard" ? "active nav-item" : "nav-item"
+            }
+          >
+            <Link href="/dashboard">
               <HomeIcon sx={{ fontSize: "28px" }} />
             </Link>
           </li>
 
           {/* Route Two */}
-          <li className={pathname == "/manage-users" ? "active nav-item" : "nav-item"}>
+          <li
+            className={
+              pathname == "/manage-users" ? "active nav-item" : "nav-item"
+            }
+          >
             <Link href="/manage-users">
               <ManageAccountsIcon sx={{ fontSize: "28px" }} />
             </Link>
           </li>
 
           {/* Route Three */}
-          <li className={pathname == "/manage-payments" ? "active nav-item" : "nav-item"}>
+          <li
+            className={
+              pathname == "/manage-payments" ? "active nav-item" : "nav-item"
+            }
+          >
             <Link href="/manage-payments">
               <MonetizationOnIcon sx={{ fontSize: "28px" }} />
             </Link>
@@ -70,24 +71,27 @@ const Sidebar = () => {
 
           {/* Route four */}
 
-          <li className={pathname == "/manage-progress" ? "active nav-item" : "nav-item"}>
+          <li
+            className={
+              pathname == "/manage-progress" ? "active nav-item" : "nav-item"
+            }
+          >
             <Link href="/manage-progress">
               <WidgetsIcon sx={{ fontSize: "28px" }} />
             </Link>
           </li>
         </ul>
 
+        
+      </div>
 
-        {/* logout button */}
-        <span>
-        <LogoutIcon
-            sx={{ color: "white", fontSize: "28px", margin: "0px 25px" }}
+      {/* logout button */}
+      <span className="hover:cursor-pointer">
+          <LogoutIcon
+            sx={{ color: "white", fontSize: "28px", margin: "0px 30px 50px" }}
           />
         </span>
-        
-       
-      </div>
-    </Box>
+    </div>
   );
 };
 export default Sidebar;

6aa3a50d62abc24a25876081aeaee8dfc9fbf8fb
Aurnab <zubayeraurnab@gmail.com>
2024-02-17 09:38:37 +0600

    Blog details page updated

--- src/components/BlogDetailsPage/BlogDetailsPage.jsx
+++ src/components/BlogDetailsPage/BlogDetailsPage.jsx
@@ -3,6 +3,7 @@ import Comments from '@/components/Comments/Comments';
 import Image from 'next/image';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import MoreBlog from './MoreBlog';
 
 const BlogDetailsPage = () => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -25,27 +26,35 @@ const BlogDetailsPage = () => {
         getBlog();
     }, [count, id]);
     return (
-        <div >
+        <div className=''>
             <div className="bg-black h-16"></div>
-            <div className='px-4 md:w-4/5 mx-auto'>
-                <div className=''>
-                    <h1 className='text-6xl text-center my-4 italic font-bold'>{blog?.title}</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-12'>
+                <div className='px-4 md:w-4/5 mx-auto col-span-8 '>
+                    <div className=''>
+                        <h1 className='text-5xl text-center my-4 italic font-bold'>{blog?.title}</h1>
+                    </div>
+                    <div className='flex justify-center my-4'>
+                        <Image width={"1000"} height={"800"} src={blog?.image} alt="Card Image" className="w-auto h-auto rounded-2xl " />
+                    </div>
+                    <div className='my-10'>
+                        <p className='md:text-xl' style={{
+                            wordSpacing: "5px",
+                            textAlign: "justify",
+                            whiteSpace: "pre-line"
+                        }}>{blog?.description}</p>
+                    </div>
+                    {/* comments section */}
+                    <div>
+                        <Comments id={blog?._id} comments={blog?.blogComments} incrementCount={incrementCount} />
+                    </div>
+                    {/* comments section */}
                 </div>
-                <div className='flex justify-center my-4'>
-                    <Image width={"1000"} height={"800"} src={blog?.image} alt="Card Image" className="w-auto h-auto rounded-2xl " />
+                <div className=' col-span-4 hidden lg:block  w-full '>
+                    <h1 className='text-2xl text-center my-4 italic font-bold '>More...</h1>
+                    <div>
+                        <MoreBlog />
+                    </div>
                 </div>
-                <div className='my-10'>
-                    <p className='md:text-xl' style={{
-                        wordSpacing: "5px",
-                        textAlign: "justify",
-                        whiteSpace: "pre-line"
-                    }}>{blog?.description}</p>
-                </div>
-                {/* comments section */}
-                <div>
-                    <Comments id={blog?._id} comments={blog?.blogComments} incrementCount={incrementCount} />
-                </div>
-                {/* comments section */}
             </div>
 
         </div>

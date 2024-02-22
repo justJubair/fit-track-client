56eb89796dbbc7845194d539e2518fc5cbca848e
Arnab Saha <arnabs929@gmail.com>
2024-02-22 11:22:39 +0600

    updating module

--- src/components/Module/Module.jsx
+++ src/components/Module/Module.jsx
@@ -10,10 +10,11 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';;
 import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
+import PrivateRoute from "../Private/PrivateRoute";
 
 const Module = ({ module }) => {
     const router = useRouter();
-    console.log(module)
+
     const defVideo = module.videos[0].vlink;
 
 
@@ -27,9 +28,6 @@ const Module = ({ module }) => {
     const handleModule = async (vid, vidIndex) => {
         setCurrentVideo('')
         setCurrentVideo(vid.vlink)
-
-
-
         setVideoIndex(vidIndex)
     }
 
@@ -113,63 +111,77 @@ const Module = ({ module }) => {
         setValue(newValue);
     };
 
+    //Post feed back on database
+    const handleFeedBackFrom = (e) => {
+        e.preventDefault();
+        const feedback = form.feedback.value;
+        console.log(feedback)
+        const feedBackData = {
+            email: session.user.email,
+        }
+    }
     return (
-        <div className=" flex flex-col lg:flex-row gap-4 my-4 mx-8">
-            <div className="w-full">
-                <iframe className="rounded-md" width="100%" height="400" src={currentVideo} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                <div className="flex justify-between gap-4">
-                    <button onClick={handlePrev} className="bg-[#252525] px-8 py-3 my-6 text-white w-full md:w-1/5 rounded-[30px] hover:bg-[#378ae5] transition-all">👈 Previous</button>
-                    <button onClick={handleNext} className="bg-[#252525] px-8 py-3 my-6 text-white w-full md:w-1/5 rounded-[30px] hover:bg-[#378ae5] transition-all">Next 👉</button>
+        <PrivateRoute>
+            <div className=" flex flex-col lg:flex-row gap-4 my-4 mx-8">
+                <div className="w-full">
+                    <iframe className="rounded-md" width="100%" height="400" src={currentVideo} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                    <div className="flex justify-between gap-4">
+                        <button onClick={handlePrev} className="bg-[#252525] px-8 py-3 my-6 text-white w-full md:w-1/5 rounded-[30px] hover:bg-[#378ae5] transition-all">👈 Previous</button>
+                        <button onClick={handleNext} className="bg-[#252525] px-8 py-3 my-6 text-white w-full md:w-1/5 rounded-[30px] hover:bg-[#378ae5] transition-all">Next 👉</button>
+                    </div>
+                    <div>
+                        <Box sx={{ width: '100%', typography: 'body1' }}>
+                            <TabContext value={value}>
+                                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                                    <TabList onChange={handleChange} aria-label="lab API tabs example">
+                                        <Tab label="Overview" value="1" />
+                                        <Tab label="Timer" value="2" />
+                                        <Tab label="Feedback" value="3" />
+                                        <Tab label="Review" value="4" />
+                                    </TabList>
+                                </Box>
+                                <TabPanel value="1"><p className=" leading-6">{module.overview}</p></TabPanel>
+                                <TabPanel value="2">
+                                    <div className="block mx-auto text-center border-black border-[1px] py-5 rounded-md bg-black">
+                                        <Timer expiryTimestamp={time} className='text-white' />
+                                    </div>
+                                </TabPanel>
+                                <TabPanel value="3">
+                                    <div className="text-center my-8 flex flex-col">
+                                        <h1 className="text-3xl w-full text-start mx-auto font-semibold">Please, share your feedback about this module. Your Feedback will help use to make better our service.</h1>
+                                        <form onSubmit={handleFeedBackFrom}>
+                                            <textarea className="mx-auto my-8 border-black border-[1px] w-full h-44 rounded-md p-4" name="feedback" placeholder="Write your feedback here..." />
+                                            <button className="bg-[#252525] px-8 py-3 my-4 mx-auto text-white w-full md:w-1/5 rounded-[30px] hover:bg-[#378ae5] transition-all">Submit 👉</button>
+                                        </form>
+                                    </div>
+                                </TabPanel>
+                                <TabPanel value='4'>
+                                    Users Review
+                                </TabPanel>
+                            </TabContext>
+                        </Box>
+                    </div>
                 </div>
-                <div>
-                    <Box sx={{ width: '100%', typography: 'body1' }}>
-                        <TabContext value={value}>
-                            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                                <TabList onChange={handleChange} aria-label="lab API tabs example">
-                                    <Tab label="Overview" value="1" />
-                                    <Tab label="Timer" value="2" />
-                                    <Tab label="Feedback" value="3" />
-                                    <Tab label="Review" value="4" />
-                                </TabList>
-                            </Box>
-                            <TabPanel value="1"><p className=" leading-6">{module.overview}</p></TabPanel>
-                            <TabPanel value="2">
-                                <div className="block mx-auto text-center border-black border-[1px] py-5 rounded-md bg-black">
-                                    <Timer expiryTimestamp={time} className='text-white' />
-                                </div>
-                            </TabPanel>
-                            <TabPanel value="3">
-                                <div className="text-center my-24 flex flex-col">
-                                    <h1 className="text-3xl w-4/5 text-center mx-auto font-semibold">Please, share your feedback about this module. Your Feedback will help use to make better our service.</h1>
-                                    <textarea className="mx-auto my-8 border-black border-[1px] w-4/5 h-44 rounded-md p-4" placeholder="Write your feedback here..." />
-                                    <button className="bg-[#252525] px-8 py-3 my-4 mx-auto text-white w-full md:w-1/5 rounded-[30px] hover:bg-[#378ae5] transition-all">Submit 👉</button>
-                                </div>
-                            </TabPanel>
-                            <TabPanel value='4'>
-                                Users Review
-                            </TabPanel>
-                        </TabContext>
-                    </Box>
+                <div className="w-full lg:w-[40%] px-4 rounded-md h-fit">
+                    <h1 className="text-3xl font-extrabold mb-4">{module.heading}</h1>
+                    <ul>
+                        {
+                            module.videos.map((vid, vidIndex) => <li
+                                key={vid._id}
+                                className={`flex text-lg text-black border-gray border-[1px] mb-5 p-2 rounded-md cursor-pointer hover:bg-[#378ae5] hover:text-[#fff] transition-all ${unlockedVideos.includes(vidIndex) ? '' : 'opacity-50 pointer-events-none'}`}
+                                onClick={() => { handleModule(vid, vidIndex) }}>
+                                <span>
+
+                                    <CheckRoundedIcon className={` text-[#378ae5] transition-all ${unlockedVideos.includes(vidIndex) ? '' : 'opacity-50 pointer-events-none'}`} />
+                                </span>
+                                {vidIndex + 1}. {vid.title}
+                            </li>)
+                        }
+                    </ul>
                 </div>
             </div>
-            <div className="w-full lg:w-[40%] px-4 rounded-md h-fit">
-                <h1 className="text-3xl font-extrabold mb-4">{module.heading}</h1>
-                <ul>
-                    {
-                        module.videos.map((vid, vidIndex) => <li
-                            key={vid._id}
-                            className={`flex text-lg text-black border-gray border-[1px] mb-5 p-2 rounded-md cursor-pointer hover:bg-[#378ae5] hover:text-[#fff] transition-all ${unlockedVideos.includes(vidIndex) ? '' : 'opacity-50 pointer-events-none'}`}
-                            onClick={() => { handleModule(vid, vidIndex) }}>
-                            <span>
-          
-                              <CheckRoundedIcon  className={` text-[#378ae5] transition-all ${unlockedVideos.includes(vidIndex) ? '' : 'opacity-50 pointer-events-none'}`}/>
-                            </span>
-                            {vidIndex + 1}. {vid.title}
-                        </li>)
-                    }
-                </ul>
-            </div>
-        </div>
+        </PrivateRoute>
+
     );
 };
 
